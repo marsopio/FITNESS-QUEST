951810733e4ec19a247590008d18258fccf9ecab
marsopio <etemmalikcalisir@gmail.com>
2026-04-08 16:21:37 +0200

    Update service worker caching strategy and assets

--- sw.js
+++ sw.js
@@ -1,23 +1,36 @@
-const CACHE_NAME = 'fit-quest-v1';
-const assets = [
-'"'",
-'index.html',
-'manifest.json',
-'music.mp3'
+const CACHE_NAME = 'fq-overdrive-v1';
+const ASSETS_TO_CACHE = [
+  './index.html',
+  './manifest.json',
+  './music.mp3',
+  './assets/icon.png'
 ];
-// Installation du cache
-self.addEventListener('install', (e) => {
-e.waitUntil(
-caches.open(CACHE_NAME).then((cache) => {
-return cache.addAll(assets);
-})
-);
+
+// Installation : on met tout en cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
 });
-// Stratégie: On cherche dans le cache, sinon on va sur le réseau
-self.addEventListener('fetch', (e) => {
-e.respondWith(
-caches.match(e.request).then((response) => {
-return response || fetch(e.request);
-})
-);
+
+// Activation : on nettoie les anciens caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Stratégie : Cache First (on regarde le cache, sinon on va sur le réseau)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
